7d9a500a9bd7646f4e205c72f32df80eeeab75c5
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2019-08-28 22:55:13 +0100

    Script to test emulator error on a single test point

--- scripts/compare_emulators.py
+++ scripts/compare_emulators.py
@@ -21,10 +21,15 @@ assert ('LYA_EMU_REPO' in os.environ),'export LYA_EMU_REPO'
 repo=os.environ['LYA_EMU_REPO']
 
 ## Using the IGM only emulator
-basedir=repo+'/p1d_emulator/sim_suites/emulator_512_18062019'
+#basedir=repo+'/p1d_emulator/sim_suites/emulator_512_18062019'
+#skewers_label=None
+
+basedir=repo+"/p1d_emulator/sim_suites/emulator_256_15072019/"
+skewers_label='Ns256_wM0.05'
 
 ## Set up arxiv outside of the emulator object
 archive=p1d_arxiv.ArxivP1D(basedir,max_arxiv_size=arxivSize,verbose=False,
+                                skewers_label=skewers_label,
                                 drop_tau_rescalings=dropRescalings,
                                 drop_temp_rescalings=dropRescalings)
 
@@ -34,14 +39,16 @@ test=archive.data.pop(np.random.randint(len(archive.data)))
 paramList=["Delta2_p","mF","sigT_Mpc","gamma","kF_Mpc"]
 
 emu_kGP=gp_emulator.GPEmulator(basedir=basedir,
+                                    skewers_label=skewers_label,
                                    kmax_Mpc=kMax,train=True,
                                    paramList=paramList,
                                    emu_type="k_bin",
                                    passArxiv=archive)
 emu_kGP.saveEmulator()
 
-'''
+
 emu_poly=gp_emulator.GPEmulator(basedir=basedir,
+                                    skewers_label=skewers_label,
                                    kmax_Mpc=kMax,train=True,
                                    paramList=paramList,
                                    emu_type="polyfit",
@@ -54,14 +61,15 @@ parameterLimits=emu_kGP.paramLimits
 for param in paramList:
     testModel[param]=test[param]
 
-k=np.linspace(emu_kGP.arxiv.data[0]["k_Mpc"][1],emu_kGP.kmax_Mpc-0.2,100)
+k=np.linspace(emu_kGP.arxiv.data[0]["k_Mpc"][2],emu_kGP.kmax_Mpc-0.5,100)
 
 y,cov=emu_kGP.emulate_p1d_Mpc(testModel,k,return_covar=True)
 y_poly,cov_poly=emu_poly.emulate_p1d_Mpc(testModel,k,return_covar=True)
 
 plt.figure()
-plt.errorbar(k,y*k,yerr=np.sqrt(np.diag(cov)),linestyle="dashed",label="k bin emulator")
-plt.errorbar(k,y_poly*k,yerr=np.sqrt(np.diag(cov_poly)),linestyle="dashed",label="Polyfit emulator")
+plt.title("emulator_256_15072019")
+plt.errorbar(k,y*k,yerr=np.sqrt(np.diag(cov))*k,linestyle="dashed",label="k bin emulator")
+plt.errorbar(k,y_poly*k,yerr=np.sqrt(np.diag(cov_poly))*k,linestyle="dashed",label="Polyfit emulator")
 plt.plot(test["k_Mpc"],test["k_Mpc"]*test["p1d_Mpc"],label="Test point")
 #plt.yscale("log")
 plt.ylabel(r"$kP(k)_\mathrm{1D}$")
@@ -69,4 +77,3 @@ plt.xlabel(r"$k[1/Mpc]$")
 plt.xlim(0,kMax)
 plt.legend()
 plt.show()
-'''

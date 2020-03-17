dd23570d2c3c76fcdf1f2ddb1eb0a2cd12ee476a
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2020-03-17 21:39:09 +0000

    Script to generate p1d in series

---
+++ post_process/scripts/series_flux_power.py
@@ -0,0 +1,81 @@
+import numpy as np
+import argparse
+import os
+# our modules below
+import snapshot_admin
+import extract_skewers
+import measure_flux_power
+import read_gadget
+import json
+
+"""
+Script will run the final step of the postprocessing, the calculation of the
+p1d from the spectra files, done in series. This part is relatively fast
+and doesn't need to be done across multiple jobs in parallel. Also saves
+us spamming the queue with thousands of jobs
+"""
+
+# get options from command line
+parser = argparse.ArgumentParser()
+parser.add_argument('--basedir', type=str, help='Path to simulation suite',required=True)
+parser.add_argument('--n_skewers', type=int, default=10, help='Number of skewers per side',required=False)
+parser.add_argument('--width_Mpc', type=float, default=0.1, help='Cell width (in Mpc)',required=False)
+parser.add_argument('--scales_tau', type=str, default='1.0', help='Comma-separated list of optical depth scalings to use.',required=False)
+parser.add_argument('--p1d_label', type=str, default=None, help='String identifying P1D measurement and / or tau scaling.',required=False)
+args = parser.parse_args()
+
+
+scales_tau=[float(scale) for scale in args.scales_tau.split(',')]
+print('will scale tau by',scales_tau)
+
+# read information about the hypercube
+cube_json=args.basedir+"/latin_hypercube.json"
+if not os.path.isfile(cube_json):
+    raise ValueError('could not find hypercube '+cube_json)
+
+with open(cube_json) as json_data:
+    cube_data = json.load(json_data)
+
+# get number of samples in the hyper-cube
+nsamples=cube_data['nsamples']
+
+# for each sample, extract skewers for each snapshot
+for sample in range(nsamples):
+    # full path to folder for this particular simulation pair
+    pair_dir=args.basedir+'/sim_pair_'+str(sample)
+    skewers_dir=pair_dir+'/output/skewers/'
+    print('writing scripts for pair in',pair_dir)
+    for sim in ['sim_plus','sim_minus']:
+        # get redshifts / snapshots Gadget parameter file 
+        paramfile=pair_dir+'/paramfile.gadget'
+        zs=read_gadget.redshifts_from_paramfile(paramfile)
+        Nsnap=len(zs)
+        ## Loop over snapshots
+        for snap in range(Nsnap):
+            # try to read information about filtering length in simulation
+            kF_json=pair_dir+'/filtering_length.json'
+            if os.path.isfile(kF_json):
+                # read json file with filtering data
+                with open(kF_json) as json_data:
+                    kF_data = json.load(json_data)
+                kF_Mpc=kF_data['kF_Mpc'][args.snap_num]
+                print('read kF_Mpc =',kF_Mpc)
+            else:
+                kF_Mpc=None
+
+            # read file containing information of all temperature rescalings in snapshot
+            snap_filename=skewers_dir+'/'+extract_skewers.get_snapshot_json_filename(
+                        num=args.snap_num,n_skewers=args.n_skewers,
+                        width_Mpc=args.width_Mpc)
+            
+            # create an object that will deal with all skewers in the snapshot
+            snapshot=snapshot_admin.SnapshotAdmin(snap_filename,
+                                scales_tau=scales_tau,
+                                kF_Mpc=kF_Mpc)
+            # measure flux power for all tau scalings, for all temperature scalings
+            arxiv_p1d=snapshot.get_all_flux_power()
+
+            # write all measured power in a JSON file
+            snapshot.write_p1d_json(p1d_label=args.p1d_label)
+
+print('DONE')

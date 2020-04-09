71b3410d9e5a83079ad1d529f887565c0d6de541
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2020-04-09 16:08:56 +0100

    Try a different way to force

--- lya_sampler/py/emcee_sampler.py
+++ lya_sampler/py/emcee_sampler.py
@@ -133,8 +133,9 @@ class EmceeSampler(object):
                 # Check convergence
                 converged = np.all(tau * 100 < sampler.iteration)
                 converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
-                if force_steps == False and converged:
-                    break
+                if force_steps == False:
+                    if converged:
+                        break
                 old_tau = tau
 
         else:

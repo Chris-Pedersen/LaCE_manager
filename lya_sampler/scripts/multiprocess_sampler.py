3629730a6d04e6bdb635000038b30ba364167bd1
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2020-05-06 16:58:31 +0100

    New set of default sampler scripts

--- lya_sampler/scripts/multiprocess_sampler.py
+++ lya_sampler/scripts/multiprocess_sampler.py
@@ -27,11 +27,15 @@ import z_emulator
 import p1d_arxiv
 import time
 
+""" Example script to run an emcee chain """
+
+
 os.environ["OMP_NUM_THREADS"] = "1"
 
 parser = configargparse.ArgumentParser()
 parser.add_argument('-c', '--config', required=False, is_config_file=True, help='config file path')
 parser.add_argument('--basedir', help='Which emulator to load')
+parser.add_argument('--emu_type', help='k_bin or polyfit emulator')
 parser.add_argument('--skewers_label', help='Add parameter describing linear growth of structure')
 parser.add_argument('--test_sim_number',type=int, help='Which sim number to use as mock data')
 parser.add_argument('--kmax_Mpc',type=float, help='Maximum k to train emulator')
@@ -64,6 +68,11 @@ print(parser.format_values())
 print("----------")
 
 
+## Load prior volume from file
+with open("no_g_star_or_f_star.json") as json_file:
+    free_param_limits = json.load(json_file)
+print("Free parameter limits=", free_param_limits)
+
 skewers_label=args.skewers_label
 p1d_label=None
 undersample_z=args.undersample_z
@@ -71,13 +80,18 @@ paramList=None
 free_parameters=args.free_parameters
 kmax_Mpc=args.kmax_Mpc
 
-# read P1D measurement
+prior=args.prior_Gauss_rms
+if prior==-1:
+    prior=None
+
+## Generate mock P1D measurement
 data=data_MPGADGET.P1D_MPGADGET(sim_number=test_sim_number,
                                 basedir=args.basedir,
                                 skewers_label=args.skewers_label,
                                 data_cov_factor=args.data_cov_factor)
 zs=data.z
 
+## Set up emulator training data
 archive=p1d_arxiv.ArxivP1D(basedir=args.basedir,
                             drop_tau_rescalings=args.drop_tau_rescalings,z_max=args.z_max,
                             drop_sim_number=test_sim_number,nearest_tau=args.nearest_tau,
@@ -85,32 +99,34 @@ archive=p1d_arxiv.ArxivP1D(basedir=args.basedir,
                             undersample_cube=args.undersample_cube,undersample_z=args.undersample_z)
 
 
+## Set up an emulator
 if args.z_emulator==False:
     emu=gp_emulator.GPEmulator(args.basedir,p1d_label,skewers_label,z_max=args.z_max,
                                     passArxiv=archive,
-                                    verbose=False,paramList=paramList,train=True,
-                                    emu_type="k_bin", checkHulls=False,kmax_Mpc=kmax_Mpc,
+                                    verbose=False,paramList=paramList,train=False,
+                                    emu_type=args.emu_type, checkHulls=False,kmax_Mpc=kmax_Mpc,
                                     drop_tau_rescalings=args.drop_tau_rescalings,
                                     drop_temp_rescalings=args.drop_temp_rescalings,
 				    set_noise_var=args.emu_noise_var)
+    emu.load_default()
 else:
     emu=z_emulator.ZEmulator(args.basedir,p1d_label,skewers_label,z_max=args.z_max,
-                                    verbose=False,paramList=paramList,train=True,
-                                    emu_type="k_bin",passArxiv=archive,checkHulls=False,
+                                    verbose=False,paramList=paramList,train=False,
+                                    emu_type=args.emu_type,passArxiv=archive,checkHulls=False,
                                     kmax_Mpc=kmax_Mpc,
                                     drop_tau_rescalings=args.drop_tau_rescalings,
                                     drop_temp_rescalings=args.drop_temp_rescalings,
 				    set_noise_var=args.emu_noise_var)
 
-
-
+## Create likelihood object from data and emulator
 like=likelihood.Likelihood(data=data,emulator=emu,
-                            free_parameters=free_parameters,verbose=False,
-                            prior_Gauss_rms=args.prior_Gauss_rms,
+                            free_parameters=free_parameters,
+			    free_param_limits=free_param_limits,
+			    verbose=False,
+                            prior_Gauss_rms=prior,
                             emu_cov_factor=args.emu_cov_factor)
 
-#like.plot_p1d()
-
+## Pass likelihood to sampler
 sampler = emcee_sampler.EmceeSampler(like=like,
                         free_parameters=free_parameters,verbose=False,
                         nwalkers=args.nwalkers)
@@ -121,7 +137,7 @@ shutil.copy(sys.argv[2],sampler.save_directory+"/"+sys.argv[2])
 for p in sampler.like.free_params:
     print(p.name,p.value,p.min_value,p.max_value)
 
-
+## Cannot call self.log_prob using multiprocess.pool
 def log_prob(theta):
     return sampler.like.log_prob(theta)
 
@@ -132,5 +148,5 @@ end = time.time()
 multi_time = end - start
 print("Sampling took {0:.1f} seconds".format(multi_time))
 
-#sampler.write_chain_to_file()
+sampler.write_chain_to_file()
 

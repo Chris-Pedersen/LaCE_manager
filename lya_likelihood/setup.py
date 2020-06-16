56df58c95acb6f076c427bd81b688c270dfc1076
Andreu Font-Ribera <font.andreu@gmail.com>
2020-06-16 18:55:24 +0200

    moved CAMB_model to likelihood module

--- lya_likelihood/setup.py
+++ lya_likelihood/setup.py
@@ -10,6 +10,6 @@ setup(name="lya_likelihood",
     url="https://github.com/andreufont/LyaCosmoParams/tree/master/lya_likelihood",
     author="Andreu Font-Ribera",
     py_modules=['lya_theory','likelihood_parameter','likelihood','full_theory',
-                'linear_power_model','recons_cosmo'],
+                'linear_power_model','recons_cosmo','CAMB_model'],
     package_dir={'': 'py'})
 

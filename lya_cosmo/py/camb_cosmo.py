fixed computation of f(z), added approximation

lya_cosmo/py/camb_cosmo.py
@@ -106,21 +106,42 @@ def get_camb_results(pars,zs=None,kmax_Mpc=camb_kmax_Mpc):
     return camb.get_results(pars)
 
 
-def get_f_of_z(pars,zs,camb_results=None):
-    """Given a CAMB cosmology, and a set of redshifts, compute the
-        logarithmic growth rate (from f sigma_8 / sigma_8). Other inputs:
-        - camb_results: if provided, use that to speed things up. """
+def get_f_of_z(pars,zs,camb_results=None,use_approx=False):
+    """Given a cosmology and a set of redshifts, compute log growth rate.
+        By default, uses velocity variance. Other inputs:
+        - camb_results: if provided, use that to speed things up.
+        - use_approx: use Om(z)**0.55 instead. """
 
     if camb_results is None:
         camb_results = get_camb_results(pars,zs=zs)
 
-    # surprisingly, there is no option to specify the fluid (CDM+baryons?)
-    transfer = camb_results.get_matter_transfer_data()
-    s8 = transfer.sigma_8
-    # note there is a bug in the CAMB documentation (sqrt missing)
-    fs8 = np.sqrt(transfer.sigma2_vdelta_8)
+    if use_approx:
+        # use f(z) = Om(z)**0.55
+        fz=np.empty_like(zs)
+        for iz in range(len(zs)):
+            z=zs[iz]
+            Omz=camb_results.get_Omega(var='cdm',z=z)
+            Omz+=camb_results.get_Omega(var='baryon',z=z)
+            fz[iz]=Omz**0.55
+    else:
+        # surprisingly, there is no option to specify the fluid (CDM+baryons?)
+        transfer = camb_results.get_matter_transfer_data()
+        # these seem to be in reverse order (see below)
+        s8 = transfer.sigma_8
+        # note there is a bug in the CAMB documentation
+        f_s8sq = transfer.sigma2_vdelta_8
+        # compute logarithmic growth rate
+        fz = f_s8sq / s8**2
 
-    return fs8 / s8
+        # sort redshifts (input zs should go from low-z to high-z)
+        if len(zs)>1:
+            assert zs[1] > zs[0]
+            if s8[1] > s8[0]:
+                # reverse order of numpy array
+                fz = fz[::-1]
+
+    # return computed values of f(z)
+    return fz
 
 
 def get_linP_hMpc(pars,zs,camb_results=None,kmin_Mpc=camb_kmin_Mpc,
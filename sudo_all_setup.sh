3945b8948484cc8a466260b52c6d40d9aebd9610
andreufont <afont@ifae.es>
2020-08-02 13:01:14 +0200

    no need for special setup

--- sudo_all_setup.sh
+++
@@ -1,9 +0,0 @@
-for DIR in lya_sampler post_process setup_simulations lya_cosmo p1d_data lya_likelihood p1d_emulator user_interface lya_nuisance
-do
-    cd $DIR
-    sudo xargs rm < files_setup.txt
-    sudo rm build/lib/*.py
-    sudo python3 setup.py install --record files_setup.txt
-    cd ..
-done	
-

9bf2a9086603068c9fe10445134a94c5c3c78dc8
Andreu Font <font.andreu@gmail.com>
2022-05-08 08:09:29 -0700

    measure P1D from post_dir

--- lace_manager/postprocess/single_sim_scripts/archive_flux_power.py
+++ lace_manager/postprocess/single_sim_scripts/archive_flux_power.py
@@ -7,14 +7,12 @@ import json
 # our modules below
 from lace_manager.postprocess import snapshot_admin
 from lace_manager.postprocess import extract_skewers
-from lace_manager.postprocess import measure_flux_power
-from lace_manager.setup_simulations import read_gadget
 
 # get options from command line
 parser = argparse.ArgumentParser()
-parser.add_argument('--simdir', type=str, help='Base simulation directory',required=True)
+parser.add_argument('--post_dir', type=str,
+                help='Base directory with simulation post-processings',required=True)
 parser.add_argument('--snap_num', type=int, help='Snapshop number',required=True)
-parser.add_argument('--skewers_dir', type=str, help='Store skewers in this folder',required=False)
 parser.add_argument('--n_skewers', type=int, default=10, help='Number of skewers per side',required=False)
 parser.add_argument('--width_Mpc', type=float, default=0.1, help='Cell width (in Mpc)',required=False)
 parser.add_argument('--scales_tau', type=str, default='1.0', help='Comma-separated list of optical depth scalings to use.',required=False)
@@ -25,19 +23,14 @@ args = parser.parse_args()
 verbose=args.verbose
 print('verbose =',verbose)
 
-# main simulation folder 
-simdir=args.simdir
-if args.skewers_dir:
-    skewers_dir=args.skewers_dir
-else:
-    skewers_dir=simdir+'/output/skewers/'
+post_dir=args.post_dir
 
 scales_tau=[float(scale) for scale in args.scales_tau.split(',')]
 if verbose:
     print('will scale tau by',scales_tau)
 
 # try to read information about filtering length in simulation
-kF_json=simdir+'/filtering_length.json'
+kF_json=post_dir+'/filtering_length.json'
 if os.path.isfile(kF_json):
     # read json file with filtering data
     with open(kF_json) as json_data:
@@ -48,7 +41,7 @@ else:
     kF_Mpc=None
 
 # read file containing information of all temperature rescalings in snapshot
-snap_filename=skewers_dir+'/'+extract_skewers.get_snapshot_json_filename(
+snap_filename=post_dir+'/skewers/'+extract_skewers.get_snapshot_json_filename(
                 num=args.snap_num,n_skewers=args.n_skewers,
                 width_Mpc=args.width_Mpc)
 if verbose:
@@ -56,13 +49,13 @@ if verbose:
 
 # create an object that will deal with all skewers in the snapshot
 snapshot=snapshot_admin.SnapshotAdmin(snap_filename,scales_tau=scales_tau,
-                                                            kF_Mpc=kF_Mpc)
+                                    kF_Mpc=kF_Mpc,post_dir=post_dir)
 Nsk=len(snapshot.data['sk_files'])
 if verbose:
     print('snapshot has {} temperature rescalings'.format(Nsk))
 
 # measure flux power for all tau scalings, for all temperature scalings
-archive_p1d=snapshot.get_all_flux_power(simdir)
+archive_p1d=snapshot.get_all_flux_power()
 
 # write all measured power in a JSON file
 snapshot.write_p1d_json(p1d_label=args.p1d_label)
